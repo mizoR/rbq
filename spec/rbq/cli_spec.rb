a334827163c256fce73a800a4ae5e4e46326de8e
mizokami <r.mizokami@gmail.com>
2015-08-11 14:50:22 +0900

    Add spec for input as YAML

--- spec/rbq/cli_spec.rb
+++ spec/rbq/cli_spec.rb
@@ -1,7 +1,50 @@
 require 'spec_helper'
 
 describe Rbq::CLI do
-  subject { cli }
+  shared_examples_for 'dump as JSON' do |options|
+    context "when extraction the languages' name of 4-characters" do
+      let(:argv)   { ['--from', options[:from], script] }
+      let(:script) { 'select {|language| language["lang"].length == 4}.map {|language| language["lang"]}' }
+
+      it { is_expected.to eq JSON.pretty_generate(%w|Java Perl Ruby|) }
+    end
+  end
+
+  shared_examples_for 'dump as YAML' do |options|
+    context 'when detection the language name born the earliest, and dump as YAML' do
+      let(:argv)   { ['--from', options[:from], '--to', 'yaml', script] }
+      let(:script) { 'max_by {|language| language["born_in"]}' }
+
+      it { is_expected.to eq YAML.dump('lang' => 'C#', 'born_in' => 2000, 'inspired_by' => %w|Delphi Java C++ Ruby|) }
+    end
+  end
+
+  shared_examples_for 'dump as CSV' do |options|
+    context "when extraction the languages inspired over 4 languages, and dump as CSV" do
+      let(:argv)   { ['--from', options[:from], '--to', 'csv', script] }
+      let(:script) { 'select {|language| language["inspired_by"].length >= 4}.map {|language| [language["lang"], language["inspired_by"].count]}' }
+
+      it do
+        is_expected.to eq CSV.generate {|csv|
+          csv << ["C#",         4]
+          csv << ["JavaScript", 4]
+          csv << ["Perl",       5]
+          csv << ["Ruby",       4]
+        }
+      end
+    end
+  end
+
+  shared_examples_for 'dump as String' do |options|
+    context "when Ruby was born in?" do
+      let(:argv)   { ['--from', options[:from], '--to', 'string', script] }
+      let(:script) { 'detect {|language| language["lang"] == "Ruby"}.slice("born_in").flatten.unshift("Ruby was").join(" ").gsub(/_/, " ")' }
+
+      it do
+        is_expected.to eq 'Ruby was born in 1995'
+      end
+    end
+  end
 
   before do
     allow(STDOUT).to receive(:tty?).and_return(false)
@@ -20,7 +63,7 @@ describe Rbq::CLI do
       {lang: 'PHP',        born_in: 1995, inspired_by: %w|Perl C|},
       {lang: 'Python',     born_in: 1991, inspired_by: %w|ABC Perl Modula-3|},
       {lang: 'Ruby',       born_in: 1995, inspired_by: %w|Perl Smalltalk LISP CLU|},
-    ]
+    ].map(&:stringify_keys)
   end
 
   describe '#run' do
@@ -29,42 +72,25 @@ describe Rbq::CLI do
     describe 'import as JSON' do
       let(:data)   { JSON.generate(languages) }
 
-      context "when extraction the languages' name of 4-characters" do
-        let(:argv)   { [script] }
-        let(:script) { 'select {|language| language["lang"].length == 4}.map {|language| language["lang"]}' }
+      it_behaves_like 'dump as JSON', from: 'json'
 
-        it { is_expected.to eq JSON.pretty_generate(%w|Java Perl Ruby|) }
-      end
+      it_behaves_like 'dump as YAML', from: 'json'
 
-      context 'when detection the language name born the earliest, and dump as YAML' do
-        let(:argv)   { ['--to', 'yaml', script] }
-        let(:script) { 'max_by {|language| language["born_in"]}' }
+      it_behaves_like 'dump as CSV', from: 'json'
 
-        it { is_expected.to eq YAML.dump('lang' => 'C#', 'born_in' => 2000, 'inspired_by' => %w|Delphi Java C++ Ruby|) }
-      end
+      it_behaves_like 'dump as String', from: 'json'
+    end
 
-      context "when extraction the languages inspired over 4 languages, and dump as CSV" do
-        let(:argv)   { ['--to', 'csv', script] }
-        let(:script) { 'select {|language| language["inspired_by"].length >= 4}.map {|language| [language["lang"], language["inspired_by"].count]}' }
+    describe 'import as YAML' do
+      let(:data)   { YAML.dump(languages) }
 
-        it do
-          is_expected.to eq CSV.generate {|csv|
-            csv << ["C#",         4]
-            csv << ["JavaScript", 4]
-            csv << ["Perl",       5]
-            csv << ["Ruby",       4]
-          }
-        end
-      end
+      it_behaves_like 'dump as JSON', from: 'yaml'
 
-      context "when Ruby was born in?" do
-        let(:argv)   { ['--to', 'string', script] }
-        let(:script) { 'detect {|language| language["lang"] == "Ruby"}.slice("born_in").flatten.unshift("Ruby was").join(" ").gsub(/_/, " ")' }
+      it_behaves_like 'dump as YAML', from: 'yaml'
 
-        it do
-          is_expected.to eq 'Ruby was born in 1995'
-        end
-      end
+      it_behaves_like 'dump as CSV', from: 'yaml'
+
+      it_behaves_like 'dump as String', from: 'yaml'
     end
   end
 end
