fdaf794452ec4539b10f0c4130b40c0a69f3c705
mizokami <r.mizokami@gmail.com>
2015-08-13 16:08:38 +0900

    DRY TSV!

--- lib/rbq/middleware/deserialize/tsv.rb
+++ lib/rbq/middleware/deserialize/tsv.rb
@@ -1,17 +1,9 @@
-require 'csv'
-
 module Rbq
   module Middleware
     module Deserialize
-      class TSV
+      class TSV < CSV
         def initialize(app, options={})
-          @app = app
-          @options = options.reverse_merge(col_sep: "\t")
-        end
-
-        def call(data)
-          data = ::CSV.parse(data, @options)
-          @app.call(data)
+          super app, options.reverse_merge(col_sep: "\t")
         end
       end
     end
--- lib/rbq/middleware/serialize/tsv.rb
+++ lib/rbq/middleware/serialize/tsv.rb
@@ -1,26 +1,9 @@
-require 'csv'
-
 module Rbq
   module Middleware
     module Serialize
-      class TSV
+      class TSV < CSV
         def initialize(app, options={})
-          @app = app
-          @options = options.reverse_merge(col_sep: "\t")
-        end
-
-        def call(data)
-          data = @app.call(data)
-          generate(data, @options)
-        end
-
-        private
-
-        def generate(data, options)
-          ::CSV.generate('', options) do |csv|
-            data = Array.wrap(data)
-            data.each {|row| csv << Array.wrap(row) }
-          end
+          super app, options.reverse_merge(col_sep: "\t")
         end
       end
     end
