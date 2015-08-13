5ed6c0dc1846af029b093126eda596832c033587
mizokami <r.mizokami@gmail.com>
2015-08-13 19:36:43 +0900

    Add command options `--{from,to}-options`

--- lib/rbq/option_parser.rb
+++ lib/rbq/option_parser.rb
@@ -13,16 +13,21 @@ module Rbq
 
     DEFAULT_FORMAT = 'json'.freeze
 
+    RE_NUMBER = %r|\A[1-9][0-9]*\z|
+    RE_OPTION = %r|\A(\S+):(\S+)\z|
+
     def initialize
       super do |opt|
         opt.banner  = BANNER
         opt.version = Rbq::VERSION
         opt.separator SEPARATOR
 
-        opt.on(      '--format  FORMAT', 'Load and dump format') {|v| @options[:from][:format] = @options[:to][:format] = v}
-        opt.on(      '--from    FORMAT', 'Load format') {|v| @options[:from][:format] = v}
-        opt.on(      '--to      FORMAT', 'Dump format') {|v| @options[:to][:format] = v}
-        opt.on('-r', '--require LIBRARY', 'Require specified library') {|v| require v}
+        opt.on(      '--format       FORMAT', 'Load and dump format') {|v| @options[:from][:format] = @options[:to][:format] = v}
+        opt.on(      '--from         FORMAT', 'Load format') {|v| @options[:from][:format] = v}
+        opt.on(      '--to           FORMAT', 'Dump format') {|v| @options[:to][:format] = v}
+        opt.on(      '--from-options FORMAT', 'Load format options') {|v| merge_options!(@options[:from][:options], v)}
+        opt.on(      '--to-options   FORMAT', 'Dump format options') {|v| merge_options!(@options[:to][:options], v)}
+        opt.on('-r', '--require      LIBRARY', 'Require specified library') {|v| require v}
       end
     end
 
@@ -32,6 +37,26 @@ module Rbq
         to:   { format: DEFAULT_FORMAT, options: {} },
       }
       super(argv) + [@options]
+    end
+
+    private
+
+    def merge_options!(options, value)
+      if value =~ RE_OPTION
+        key, value = $1, $2
+        options.merge!(key.to_sym => cast(value))
+      else
+        warn "Option was ignored. (value: #{value.inspect})"
+      end
+    end
+
+    def cast(value)
+      case value
+      when 'true'    then true
+      when 'false'   then false
+      when RE_NUMBER then value.to_i
+      else value
+      end
     end
   end
 end
